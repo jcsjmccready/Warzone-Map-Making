7c6b64e576725c88125237b1380437acc7ef95bc
jcsjmccready <a@b.c>
2026-06-19 19:23:34 +0100

    Cleaned up saving settings logic

--- Mods/DeadManSwitch/Client_SaveConfigureUI.lua
+++ Mods/DeadManSwitch/Client_SaveConfigureUI.lua
@@ -1,34 +1,46 @@
 function Client_SaveConfigureUI(alert, addCard)
-    Mod.Settings.NumPieces = numPieces.GetValue();
-    Mod.Settings.CardWeight = cardWeight.GetValue();
-    Mod.Settings.MinPieces = minPieces.GetValue();
-    Mod.Settings.InitialPieces = initialPieces.GetValue();
+
     Mod.Settings.isAcquiringTypeCard = isAcquiringTypeCard.GetIsChecked();
+    if(Mod.Settings.isAcquiringTypeCard) then
+        Mod.Settings.NumPieces = numPieces.GetValue();
+        Mod.Settings.CardWeight = cardWeight.GetValue();
+        Mod.Settings.MinPieces = minPieces.GetValue();
+        Mod.Settings.InitialPieces = initialPieces.GetValue();
+        
+        if (Mod.Settings.NumPieces < 1) then
+            alert("Number of pieces cannot be less than 1");
+            return;
+        end
+        if (Mod.Settings.CardWeight < 0) then
+            alert("Card weight cannot be less than 0");
+            return;
+        end
+        if (Mod.Settings.MinPieces < 0) then
+            alert("Minimum pieces cannot be less than 0");
+            return;
+        end
+        if (Mod.Settings.InitialPieces < 0) then
+            alert("Initial pieces cannot be less than 0");
+            return;
+        end
+
+    addCard("Dead Man's Switch Card", "Play this card to create a Dead Man's Switch on any territory you control (at the end of the turn). If this territory is taken, a bomb is played on it.", "DmsCard.png", Mod.Settings.NumPieces, Mod.Settings.MinPieces, Mod.Settings.InitialPieces, Mod.Settings.CardWeight);
+    end
+
     Mod.Settings.isAcquiringTypeCommerce = isAcquiringTypeCommerce.GetIsChecked();
+
     Mod.Settings.isDamageTypeBomb = isDamageTypeBomb.GetIsChecked();
+    
     Mod.Settings.isDamageTypeFlat = isDamageTypeFlat.GetIsChecked();
+    if(Mod.Settings.isDamageTypeFlat) then
+        Mod.Settings.FlatDamage = flatDamage.GetValue();
+    end
+
     Mod.Settings.isDamageTypePercent = isDamageTypePercent.GetIsChecked();
-    Mod.Settings.FlatDamage = flatDamage.GetValue();
-    Mod.Settings.PercentageDamage = percentageDamage.GetValue();
-    Mod.Settings.PercentageMinDamage = percentageMinDamage.GetValue();
-
-    if (Mod.Settings.NumPieces < 1) then
-        alert("Number of pieces cannot be less than 1");
-        return;
-    end
-    if (Mod.Settings.CardWeight < 0) then
-        alert("Card weight cannot be less than 0");
-        return;
-    end
-    if (Mod.Settings.MinPieces < 0) then
-        alert("Minimum pieces cannot be less than 0");
-        return;
-    end
-    if (Mod.Settings.InitialPieces < 0) then
-        alert("Initial pieces cannot be less than 0");
-        return;
+    if(Mod.Settings.isDamageTypePercent) then
+        Mod.Settings.PercentageDamage = percentageDamage.GetValue();
+        Mod.Settings.PercentageMinDamage = percentageMinDamage.GetValue();
     end
 
-    local cardID = addCard("Dead Man's Switch Card", "Play this card to create a Dead Man's Switch on any territory you control (at the end of the turn). If this territory is taken, a bomb is played on it.", "DmsCard.png", Mod.Settings.NumPieces, Mod.Settings.MinPieces, Mod.Settings.InitialPieces, Mod.Settings.CardWeight);
 end
 
