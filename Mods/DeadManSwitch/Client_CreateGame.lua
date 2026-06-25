df8e20d89d241448565328cc5ff9a1bf3e44cc61
jcsjmccready <a@b.c>
2026-06-25 23:14:08 +0100

    Removing bomb constraint when bomb not required for dms

--- Mods/DeadManSwitch/Client_CreateGame.lua
+++ Mods/DeadManSwitch/Client_CreateGame.lua
@@ -2,7 +2,7 @@
 ---@param settings GameSettings # Read-only GameSettings object
 ---@param alert fun(message: string) # When invoked, it will show a pop-up for the client with the message. It will also abort the game creation
 function Client_CreateGame(settings, alert)
-    if settings.Cards == nil or settings.Cards[WL.CardID.Bomb] == nil then
+    if (Mod.Settings.isDamageTypeBomb and (settings.Cards == nil or settings.Cards[WL.CardID.Bomb] == nil)) then
         alert("Bombs must be enabled for this mod to work.");
     end
 end
